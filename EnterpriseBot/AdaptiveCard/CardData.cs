f04816ce6af22ba432bc807260ff0a7ff294aafd
yuesu <yuesu@microsoft.com>
2018-12-27 10:46:39 +0800

    add adaptive card

---
+++ EnterpriseBot/AdaptiveCard/CardData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Bot.Solutions.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnterpriseBot.AdaptiveCard
+{
+    public class CardData : CardDataBase
+    {
+
+    }
+}
